d1a87e7597d599bd3e9a8215ac7688d97c1fc10f
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-07-04 01:35:59 +0200

    add test skeleton

---
+++ src/test/java/hello/CommercetoolsHandlePaymentsControllerTest.java
@@ -0,0 +1,36 @@
+package hello;
+
+import com.commercetools.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class CommercetoolsHandlePaymentsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnTenantAndPayment() throws Exception {
+
+        this.mockMvc.perform(get("/GKI/commercetools/handle/payments/XXX-YYY"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tenantName").value("GKI"))
+                .andExpect(jsonPath("$.paymentId").value("Hello, payment [XXX-YYY]!"));
+    }
+
+
+}

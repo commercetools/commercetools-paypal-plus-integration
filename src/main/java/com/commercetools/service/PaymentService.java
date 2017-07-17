31ef310ee897c088be5f64218cf390b3e0b53f5c
Lam Tran <lam.tran@commercetools.de>
2017-07-17 14:48:10 +0200

    #5 implement getByPaymentMethodAndInterfaceId()

--- src/main/java/com/commercetools/service/PaymentService.java
+++ src/main/java/com/commercetools/service/PaymentService.java
@@ -1,11 +1,22 @@
 package com.commercetools.service;
 
 import io.sphere.sdk.payments.Payment;
+import io.sphere.sdk.payments.PaymentMethodInfo;
 
 import javax.annotation.Nullable;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
 public interface PaymentService {
 
     CompletionStage<Payment> getById(@Nullable String id);
+
+    /**
+     * Fetch payment with by its {@link Payment#getInterfaceId()} and {@link PaymentMethodInfo#getPaymentInterface()}.
+     *
+     * @param paymentMethodInterface name of payment interface, like "PAYONE"
+     * @param interfaceId the payment's {@link Payment#getInterfaceId()}
+     * @return completion stage with optional found payment.
+     */
+    CompletionStage<Optional<Payment>> getByPaymentMethodAndInterfaceId(String paymentMethodInterface, String interfaceId);
 }
--- src/main/java/com/commercetools/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/commercetools/service/impl/PaymentServiceImpl.java
@@ -4,11 +4,14 @@ import com.commercetools.service.PaymentService;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.payments.Payment;
 import io.sphere.sdk.payments.queries.PaymentByIdGet;
+import io.sphere.sdk.payments.queries.PaymentQuery;
+import io.sphere.sdk.queries.PagedResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
@@ -26,4 +29,17 @@ public class PaymentServiceImpl extends BaseSphereService implements PaymentServ
     public CompletionStage<Payment> getById(@Nullable String id) {
         return isBlank(id) ? completedFuture(null) : sphereClient.execute(PaymentByIdGet.of(id));
     }
+
+    @Override
+    public CompletionStage<Optional<Payment>> getByPaymentMethodAndInterfaceId(String paymentInterface, String interfaceId) {
+        if (isBlank(paymentInterface) || isBlank(interfaceId)) {
+            return completedFuture(null);
+        }
+        PaymentQuery paymentQuery = PaymentQuery.of()
+                .withPredicates(p -> p.paymentMethodInfo().paymentInterface().is(paymentInterface))
+                .plusPredicates(p -> p.interfaceId().is(interfaceId));
+        return sphereClient.execute(paymentQuery)
+                .thenApply(PagedResult::head);
+    }
+
 }

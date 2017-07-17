2bf57aad79444483628551ef78a92b390fd94332
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-07-17 09:51:56 +0200

    #17: update comment

--- src/test/java/com/commercetools/testUtil/customTestConfigs/PaymentsCleanupConfiguration.java
+++ src/test/java/com/commercetools/testUtil/customTestConfigs/PaymentsCleanupConfiguration.java
@@ -9,7 +9,10 @@ import static com.commercetools.testUtil.ctpUtil.CleanupTableUtil.cleanupPayment
 
 /**
  * <b>By importing this configuration to an <i>integration test</i> class one removes all the payments from CTP project
- * before running the tests.
+ * before running the tests.</b>
+ * <p>
+ * <b>NEVER put {@code @Configuration}, {@code @*AutoConfiguration*} and related annotations to this class to avoid
+ * database cleanup on every context loading (e.g. every test class)</b>
  */
 public class PaymentsCleanupConfiguration {
 

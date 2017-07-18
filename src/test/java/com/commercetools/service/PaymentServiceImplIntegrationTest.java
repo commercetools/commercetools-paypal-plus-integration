784b7b38abd38a21002407d08b9bfbd29a29f0cb
Lam Tran <lam.tran@commercetools.de>
2017-07-18 09:17:53 +0200

    #5 add tests for PaymentService

--- src/test/java/com/commercetools/service/PaymentServiceImplIntegrationTest.java
+++ src/test/java/com/commercetools/service/PaymentServiceImplIntegrationTest.java
@@ -3,10 +3,14 @@ package com.commercetools.service;
 import com.commercetools.Application;
 import com.commercetools.testUtil.customTestConfigs.PaymentsCleanupConfiguration;
 import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.commands.UpdateAction;
 import io.sphere.sdk.payments.Payment;
 import io.sphere.sdk.payments.PaymentDraftBuilder;
 import io.sphere.sdk.payments.PaymentDraftDsl;
+import io.sphere.sdk.payments.PaymentMethodInfo;
+import io.sphere.sdk.payments.PaymentMethodInfoBuilder;
 import io.sphere.sdk.payments.commands.PaymentCreateCommand;
+import io.sphere.sdk.payments.commands.updateactions.ChangeAmountPlanned;
 import org.javamoney.moneta.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static com.commercetools.testUtil.CompletionStageUtil.executeBlocking;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -57,5 +65,43 @@ public class PaymentServiceImplIntegrationTest {
         assertThat(executeBlocking(paymentService.getById(""))).isNull();
         assertThat(executeBlocking(paymentService.getById(" "))).isNull();
         assertThat(executeBlocking(paymentService.getById("asdfadsfasdf"))).isNull();
+    }
+
+    @Test
+    public void shouldGetPaymentByPaymentMethodAndInterfaceId() {
+        String paymentInterface = "testPaymentInterface";
+        PaymentMethodInfo testMethodInfo = PaymentMethodInfoBuilder.of()
+                .paymentInterface(paymentInterface)
+                .build();
+        String interfaceId = "testInterfaceId";
+        PaymentDraftDsl draft = PaymentDraftBuilder.of(Money.of(22.33, "EUR"))
+                .paymentMethodInfo(testMethodInfo)
+                .interfaceId(interfaceId)
+                .build();
+
+        executeBlocking(sphereClient.execute(PaymentCreateCommand.of(draft)));
+        Optional<Payment> payment = executeBlocking(
+                paymentService.getByPaymentMethodAndInterfaceId(paymentInterface, interfaceId)
+        );
+        assertThat(payment.isPresent()).isTrue();
+    }
+
+    @Test
+    public void shouldUpdateAmountPlanned() {
+        String paymentKey = "testPayment1";
+        Money amountBefore = Money.of(1, "EUR");
+        PaymentDraftDsl draft = PaymentDraftBuilder.of(amountBefore)
+                .key(paymentKey)
+                .build();
+        Payment payment = executeBlocking(sphereClient.execute(PaymentCreateCommand.of(draft)));
+
+        Money amountAfter = Money.of(2, "EUR");
+        List<UpdateAction<Payment>> updateActions = Collections.singletonList(ChangeAmountPlanned.of(amountAfter));
+        Payment updatedPayment = executeBlocking(
+                paymentService.updatePayment(payment, updateActions)
+                .thenCompose(p -> paymentService.getById(p.getId()))
+        );
+        assertThat(updatedPayment.getKey()).isEqualTo(paymentKey);
+        assertThat(updatedPayment.getAmountPlanned()).isEqualTo(amountAfter);
     }
 }

6f62455d6c86ecd7aee411dff9d18aa5eb457835
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-07-27 14:39:37 +0200

    #3: remove unused constant

--- src/main/java/com/commercetools/pspadapter/tenant/TenantLoggerUtil.java
+++ src/main/java/com/commercetools/pspadapter/tenant/TenantLoggerUtil.java
@@ -7,15 +7,10 @@ import static java.lang.String.format;
 public final class TenantLoggerUtil {
 
     /**
-     * Opposite to logger name generated in {@link #createLoggerName(Class, String)}
-     * this one is used in static common loggers where it is not possible to generate separate tenant loggers.
-     */
-    public static final String LOG_PREFIX = "[tenant {}]";
-
-    /**
      * Create a logger with name which consists of full qualified {@code clazz} name
      * and the suffix "[tenant &lt;name&gt;]".
-     * @param clazz Class where the logger is used
+     *
+     * @param clazz      Class where the logger is used
      * @param tenantName tenant name to attach to the class name.
      * @return Concatenated FQDN and the {@code tenantName} wrapped to "[tenant &lt;name&gt;]" construction.
      */

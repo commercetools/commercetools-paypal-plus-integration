8967e82c631563cc57f8debfcbc5b034108d07fa
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-07-27 11:28:33 +0200

    #3: minor formatting

--- src/main/java/com/commercetools/payment/handler/CommercetoolsHandlePaymentsController.java
+++ src/main/java/com/commercetools/payment/handler/CommercetoolsHandlePaymentsController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Nonnull;
 
@@ -34,7 +30,7 @@ public class CommercetoolsHandlePaymentsController {
             method = RequestMethod.GET,
             value = "/{tenantName}/commercetools/handle/payments/{paymentId}")
     public ResponseEntity<?> handlePayments(@PathVariable String tenantName,
-                                                             @PathVariable String paymentId) {
+                                            @PathVariable String paymentId) {
         PaymentHandleResult paymentHandleResult = paymentHandlerProvider
                 .getPaymentHandler(tenantName)
                 .map(paymentHandler -> paymentHandler.handlePayment(paymentId))

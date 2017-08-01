b18283c06735468f4f804aa47ecb0f2f1f8c0411
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-08-01 17:01:54 +0200

    #13: create docker build

---
+++ docker-build/travis-docker-publish.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+export SCRIPT_DIR=$(dirname "$0")
+
+export REPO="commercetoolsps/commercetools-paypal-plus-integration"
+export DOCKER_TAG=`if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" = "false"  ]; then echo "latest"; else echo "wip-${TRAVIS_BRANCH//\//-}" ; fi`
+
+echo "Building Docker image using tag '${REPO}:${COMMIT}'."
+docker build --file "$SCRIPT_DIR/Dockerfile"  --tag "${REPO}:${COMMIT}" .
+
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
+echo "Adding additional tag '${REPO}:${DOCKER_TAG}' to already built Docker image '${REPO}:${COMMIT}'."
+docker tag $REPO:$COMMIT $REPO:$DOCKER_TAG
+
+echo "Adding additional tag '${REPO}:travis-${TRAVIS_BUILD_NUMBER}' to already built Docker image '${REPO}:${COMMIT}'."
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+
+if [ "$TRAVIS_TAG" -a "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Adding additional tag '${REPO}:${TRAVIS_TAG}' to already built Docker image '${REPO}:${COMMIT}'."
+  docker tag $REPO:$COMMIT $REPO:${TRAVIS_TAG};
+fi
+
+echo "Pushing Docker images to repository '${REPO}' (all local tags are pushed)."
+docker push $REPO
+
+docker logout

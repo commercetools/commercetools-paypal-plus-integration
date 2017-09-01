b3cf8e3e026c525bad5bfc997e6dba02f42d6026
Lam Tran <lam.tran@commercetools.de>
2017-09-01 11:34:06 +0200

    #37 use java classes to construct expansion path

--- src/main/java/com/commercetools/payment/constants/ctp/ExpansionExpressions.java
+++ src/main/java/com/commercetools/payment/constants/ctp/ExpansionExpressions.java
@@ -1,5 +1,9 @@
 package com.commercetools.payment.constants.ctp;
 
+import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.carts.expansion.CartExpansionModel;
+import io.sphere.sdk.expansion.ExpansionPathContainer;
+
 public class ExpansionExpressions {
-    public static final String PAYMENT_INFO_EXPANSION = "paymentInfo.payments[*]";
+    public static final ExpansionPathContainer<Cart> PAYMENT_INFO_EXPANSION = CartExpansionModel.of().paymentInfo().payments();
 }

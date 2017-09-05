dea5f58375611d0cd0c017a80031653e9a400cd4
Lam Tran <lam.tran@commercetools.de>
2017-09-05 11:57:52 +0200

    #2 add comment and test to a webhook container method

--- src/main/java/com/commercetools/pspadapter/notification/webhook/WebhookContainer.java
+++ src/main/java/com/commercetools/pspadapter/notification/webhook/WebhookContainer.java
@@ -7,5 +7,10 @@ import java.util.concurrent.CompletionStage;
 
 public interface WebhookContainer {
 
+    /**
+     * Get Paypal's {@link Webhook} by the tenant's name. If none is found, returns {@code null}.
+     * @param tenantName name of the tenant
+     * @return tenant's {@link Webhook}. If none is found, {@code null} is returned.
+     */
     CompletionStage<Webhook> getWebhookCompletionStageByTenantName(@Nonnull String tenantName);
 }

fd64df4fbacc0b93259264c1c59be0495747e573
Andrii Kovalenko <akovalenko@ct-00573.local>
2017-09-29 17:24:48 +0200

    #91: implement street number mapping in address mapping

--- src/main/java/com/commercetools/helper/mapper/impl/AddressMapperImpl.java
+++ src/main/java/com/commercetools/helper/mapper/impl/AddressMapperImpl.java
@@ -4,9 +4,12 @@ import com.commercetools.helper.mapper.AddressMapper;
 import com.paypal.api.payments.PayerInfo;
 import com.paypal.api.payments.ShippingAddress;
 import io.sphere.sdk.models.Address;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 public class AddressMapperImpl implements AddressMapper {
@@ -15,7 +18,19 @@ public class AddressMapperImpl implements AddressMapper {
     public ShippingAddress ctpAddressToPaypalPlusShippingAddress(@Nonnull Address ctpAddress) {
         ShippingAddress paypalAddress = new ShippingAddress();
         paypalAddress.setRecipientName(ctpAddress.getFirstName() + " " + ctpAddress.getLastName());
-        paypalAddress.setLine1(ctpAddress.getStreetName());
+        return populateCommonAddressFields(ctpAddress, paypalAddress);
+    }
+
+    @Override
+    public com.paypal.api.payments.Address ctpAddressToPaypalPlusBillingAddress(@Nonnull Address ctpAddress) {
+        return populateCommonAddressFields(ctpAddress, new com.paypal.api.payments.Address());
+    }
+
+    private <A extends com.paypal.api.payments.Address> A populateCommonAddressFields(
+            @Nonnull Address ctpAddress,
+            @Nonnull A paypalAddress) {
+        paypalAddress.setLine1(generateLine1(ctpAddress));
+
         paypalAddress.setLine2(ctpAddress.getApartment());
         paypalAddress.setCity(ctpAddress.getCity());
         // Phone has strict verification on Paypal, for now, we don't send it
@@ -35,17 +50,17 @@ public class AddressMapperImpl implements AddressMapper {
                 .setBillingAddress(ctpAddressToPaypalPlusBillingAddress(ctpAddress));
     }
 
-    @Override
-    public com.paypal.api.payments.Address ctpAddressToPaypalPlusBillingAddress(@Nonnull Address ctpAddress) {
-        com.paypal.api.payments.Address paypalAddress = new com.paypal.api.payments.Address();
-        paypalAddress.setLine1(ctpAddress.getStreetName());
-        paypalAddress.setLine2(ctpAddress.getApartment());
-        paypalAddress.setCity(ctpAddress.getCity());
-        // Phone has strict verification on Paypal, for now, we don't send it
-//        paypalAddress.setPhone(ctpAddress.getPhone());
-        paypalAddress.setPostalCode(ctpAddress.getPostalCode());
-        paypalAddress.setState(ctpAddress.getState());
-        paypalAddress.setCountryCode(ctpAddress.getCountry().getAlpha2());
-        return paypalAddress;
+    /**
+     * @param ctpAddress CTP {@link Address} to read
+     * @return {@link Address#streetName} optionally followed by {@link Address#streetNumber}, if the street number is
+     * not blank.
+     */
+    private String generateLine1(@Nonnull Address ctpAddress) {
+        return ctpAddress.getStreetName() +
+                ofNullable(ctpAddress.getStreetNumber())
+                        .filter(StringUtils::isNotBlank)
+                        .map(n -> " " + n)
+                        .orElse("");
     }
+
 }

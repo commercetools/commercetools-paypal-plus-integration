44f5a1877064ae5bb131c3767dc5b529f2376969
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2017-12-29 12:53:31 +0100

    #40: AggregatedCtpTypesValidationResultTest: execute result tests added

--- src/test/java/com/commercetools/config/ctpTypes/AggregatedCtpTypesValidationResultTest.java
+++ src/test/java/com/commercetools/config/ctpTypes/AggregatedCtpTypesValidationResultTest.java
@@ -1,13 +1,25 @@
 package com.commercetools.config.ctpTypes;
 
+import com.commercetools.service.ctp.impl.TypeServiceImpl;
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.models.LocalizedString;
+import io.sphere.sdk.types.FieldDefinition;
+import io.sphere.sdk.types.StringFieldType;
 import io.sphere.sdk.types.Type;
+import io.sphere.sdk.types.TypeDraft;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import static com.commercetools.config.ctpTypes.AggregatedCtpTypesValidationResultTest.TypeServiceMock.typeServiceMock;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -56,10 +68,72 @@ public class AggregatedCtpTypesValidationResultTest {
         assertThat(result.getAggregatedErrorMessage()).isEmpty();
         assertThat(result.hasUpdatedTypes()).isTrue();
 
-        String concatTenantNamesTypeKeys = result.getUpdatedTypes().stream()
+        String concatTenantNamesTypeKeys = concatUpdatedTypesToString(result);
+        assertThat(concatTenantNamesTypeKeys).isEqualTo("tenant1#t1_key1,t1_key2;tenant2#t2_key1");
+    }
+
+    @Test
+    public void executeAndAggregateTenantValidationResults_withEmptyActions_shouldReturnEmptyResult() throws Exception {
+        AggregatedCtpTypesValidationResult result = AggregatedCtpTypesValidationResult.executeAndAggregateTenantValidationResults(asList(
+                TenantCtpTypesValidationAction.ofEmpty("tenant1", typeServiceMock()),
+                TenantCtpTypesValidationAction.ofEmpty("tenant2", typeServiceMock())
+        )).toCompletableFuture().join();
+
+        assertThat(result).isNotNull();
+        assertThat(result).isNotNull();
+        assertThat(result.isEmpty()).isTrue();
+        assertThat(result.hasErrorMessage()).isFalse();
+        assertThat(result.getAggregatedErrorMessage()).isEmpty();
+        assertThat(result.hasUpdatedTypes()).isFalse();
+        assertThat(result.getUpdatedTypes()).isEmpty();
+    }
+
+    @Test
+    public void executeAndAggregateTenantValidationResults_withErrorAction_shouldReturnErrorResult() throws Exception {
+        AggregatedCtpTypesValidationResult result = AggregatedCtpTypesValidationResult.executeAndAggregateTenantValidationResults(asList(
+                TenantCtpTypesValidationAction.ofEmpty("tenant1", typeServiceMock()),
+                TenantCtpTypesValidationAction.ofCreateType("tenant2", typeServiceMock(), mockType("t2_key1")),
+                TenantCtpTypesValidationAction.ofAddFieldDefinitions("tenant3", typeServiceMock(), mockType("t3_key1"),
+                        singletonList(FieldDefinition.of(StringFieldType.of(), "field1", LocalizedString.of(), false))),
+                TenantCtpTypesValidationAction.ofErrorMessages("tenant3", typeServiceMock(), asList("error1", "error2"))
+        )).toCompletableFuture().join();
+
+        assertThat(result).isNotNull();
+        assertThat(result).isNotNull();
+        assertThat(result.isEmpty()).isFalse();
+        assertThat(result.hasErrorMessage()).isTrue();
+        assertThat(result.getAggregatedErrorMessage()).contains("error1", "error2");
+        assertThat(result.hasUpdatedTypes()).isFalse();
+        assertThat(result.getUpdatedTypes()).isEmpty();
+    }
+
+    @Test
+    public void executeAndAggregateTenantValidationResults_withUpdateActions_shouldReturnUpdatedTypesList() throws Exception {
+        AggregatedCtpTypesValidationResult result = AggregatedCtpTypesValidationResult.executeAndAggregateTenantValidationResults(asList(
+                TenantCtpTypesValidationAction.ofEmpty("tenant1", typeServiceMock()),
+                TenantCtpTypesValidationAction.ofCreateType("tenant2", typeServiceMock(), mockType("t2_key1")),
+                TenantCtpTypesValidationAction.ofAddFieldDefinitions("tenant3", typeServiceMock(), mockType("t3_key1"),
+                        singletonList(FieldDefinition.of(StringFieldType.of(), "field1", LocalizedString.of(), false))),
+                TenantCtpTypesValidationAction.ofEmpty("tenant4", typeServiceMock())
+        )).toCompletableFuture().join();
+
+        assertThat(result).isNotNull();
+        assertThat(result).isNotNull();
+        assertThat(result.isEmpty()).isFalse();
+        assertThat(result.hasErrorMessage()).isFalse();
+        assertThat(result.getAggregatedErrorMessage()).isEmpty();
+        assertThat(result.hasUpdatedTypes()).isTrue();
+        assertThat(concatUpdatedTypesToString(result)).isEqualTo("tenant2#t2_key1;tenant3#t3_key1");
+    }
+
+    /**
+     * From update types list produce a string in format:
+     * {@code tenantName1#typeKey1,typeKey2;tenantName1#typeKey3,typeKey4......}
+     */
+    private static String concatUpdatedTypesToString(AggregatedCtpTypesValidationResult aggregatedResult) {
+        return aggregatedResult.getUpdatedTypes().stream()
                 .map(pair -> format("%s#%s", pair.getLeft(), pair.getRight().stream().map(Type::getKey).collect(joining(","))))
                 .collect(joining(";"));
-        assertThat(concatTenantNamesTypeKeys).isEqualTo("tenant1#t1_key1,t1_key2;tenant2#t2_key1");
     }
 
     private static Type mockType(String key) {
@@ -67,5 +141,25 @@ public class AggregatedCtpTypesValidationResultTest {
         when(type.getKey()).thenReturn(key);
 
         return type;
+    }
+
+    static class TypeServiceMock extends TypeServiceImpl {
+        TypeServiceMock() {
+            super(mock(SphereClient.class));
+        }
+
+        @Override
+        public CompletionStage<Type> createType(@Nonnull TypeDraft typeDraft) {
+            return completedFuture(mockType(typeDraft.getKey()));
+        }
+
+        @Override
+        public CompletionStage<Type> addFieldDefinitions(@Nonnull Type type, @Nonnull List<FieldDefinition> fieldDefinitions) {
+            return completedFuture(type);
+        }
+
+        static TypeServiceMock typeServiceMock() {
+            return new TypeServiceMock();
+        }
     }
 }

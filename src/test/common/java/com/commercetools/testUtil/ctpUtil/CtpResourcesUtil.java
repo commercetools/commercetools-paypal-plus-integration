1510ba35c20f88aa61aecd4201a52ddb10541376
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2018-03-19 19:44:11 +0100

    #104: minor static imports

--- src/test/common/java/com/commercetools/testUtil/ctpUtil/CtpResourcesUtil.java
+++ src/test/common/java/com/commercetools/testUtil/ctpUtil/CtpResourcesUtil.java
@@ -1,10 +1,7 @@
 package com.commercetools.testUtil.ctpUtil;
 
 import com.commercetools.model.CtpPaymentWithCart;
-import com.commercetools.payment.constants.LocaleConstants;
-import com.commercetools.payment.constants.ctp.CtpPaymentCustomFields;
 import com.commercetools.payment.constants.ctp.CtpPaymentMethods;
-import com.commercetools.payment.constants.paypalPlus.PaypalPlusPaymentInterfaceName;
 import com.commercetools.testUtil.ResourcesUtil;
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.carts.CartDraft;
@@ -33,7 +30,6 @@ import java.util.concurrent.CompletionStage;
 
 import static com.commercetools.payment.constants.LocaleConstants.DEFAULT_LOCALE;
 import static com.commercetools.payment.constants.ctp.CtpPaymentCustomFields.*;
-import static com.commercetools.payment.constants.ctp.CtpPaymentMethods.DEFAULT;
 import static com.commercetools.payment.constants.paypalPlus.PaypalPlusPaymentInterfaceName.PAYPAL_PLUS;
 import static com.commercetools.testUtil.CompletionStageUtil.executeBlocking;
 import static io.sphere.sdk.json.SphereJsonUtils.readObjectFromResource;
@@ -125,12 +121,12 @@ public class CtpResourcesUtil extends ResourcesUtil {
 
     public static PaymentDraftBuilder createPaymentDraftBuilder(@Nonnull MonetaryAmount totalPrice, @Nullable Locale locale) {
         return PaymentDraftBuilder.of(totalPrice)
-                .paymentMethodInfo(PaymentMethodInfoBuilder.of().paymentInterface(PaypalPlusPaymentInterfaceName.PAYPAL_PLUS).method(CtpPaymentMethods.DEFAULT).build())
+                .paymentMethodInfo(PaymentMethodInfoBuilder.of().paymentInterface(PAYPAL_PLUS).method(CtpPaymentMethods.DEFAULT).build())
                 .custom(CustomFieldsDraftBuilder.ofTypeKey("payment-paypal")
-                        .addObject(CtpPaymentCustomFields.SUCCESS_URL_FIELD, "http://example.com/success/23456789")
-                        .addObject(CtpPaymentCustomFields.CANCEL_URL_FIELD, "http://example.com/cancel/23456789")
-                        .addObject(CtpPaymentCustomFields.REFERENCE, "23456789")
-                        .addObject(CtpPaymentCustomFields.LANGUAGE_CODE_FIELD, ofNullable(locale).orElse(LocaleConstants.DEFAULT_LOCALE).getLanguage())
+                        .addObject(SUCCESS_URL_FIELD, "http://example.com/success/23456789")
+                        .addObject(CANCEL_URL_FIELD, "http://example.com/cancel/23456789")
+                        .addObject(REFERENCE, "23456789")
+                        .addObject(LANGUAGE_CODE_FIELD, ofNullable(locale).orElse(DEFAULT_LOCALE).getLanguage())
                         .build());
     }
 

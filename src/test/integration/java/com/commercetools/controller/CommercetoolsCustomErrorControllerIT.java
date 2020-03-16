25059ae8c7164141589dc8dd8520480f5b92bb91
leungkinghin <edison.khleung@gmail.com>
2020-03-16 11:43:09 +0100

    Change the source package of LocalServerPort class

--- src/test/integration/java/com/commercetools/controller/CommercetoolsCustomErrorControllerIT.java
+++ src/test/integration/java/com/commercetools/controller/CommercetoolsCustomErrorControllerIT.java
@@ -4,7 +4,7 @@ import com.commercetools.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;

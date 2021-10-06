635481f2fb1e3fbf483dd5d888cc50bb1ef64a59
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2021-10-06 09:38:35 +0200

    Add solutioninfo to correct folder.

--- src/main/java/com/commercetools/util/PayPalSolutionInfo.java
+++ src/main/java/com/commercetools/util/PayPalSolutionInfo.java
@@ -13,7 +13,7 @@ public final class PayPalSolutionInfo extends SolutionInfo {
    * information of the commercetools-paypal-plus-integration library
    *
    * <p>A User-Agent header with a solution information looks like this: {@code
-   * commercetools-jvm-sdk/1.46.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64) {@value
+   * commercetools-paypal-plus-integration/1.46.1 (AHC/2.0) Java/1.8.0_92-b14 (Mac OS X; x86_64) {@value
    * LIB_NAME}/{@value LIB_VERSION}}
    */
   public PayPalSolutionInfo() {
